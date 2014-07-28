4490899c4666d6562858799ea80fafa12aced56d
unknown <Wilson@Wilson-PC.(none)>
2014-07-28 01:02:47 -0700

    Adding a command line parser library sandbox

---
+++ src/CommandLineParser/CommandLineParser/Options.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using CommandLine;
+using CommandLine.Text;
+
+namespace CommandLineParser
+{
+    public class Options
+    {
+        [Option('t', "test", Required = true, HelpText = "Test Option")]
+        public string Test { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            return HelpText.AutoBuild(this, current => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+    }
+}
---
+++ src/CommandLineParser/CommandLineParser/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CommandLineParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = new Options();
+            var argsParsed = CommandLine.Parser.Default.ParseArguments(args, options);
+
+            if ((!string.Equals(args[0], "--help") && !string.Equals(args[0], "-h")) && args.Length == 1)
+            {
+                if (!argsParsed)
+                    throw new ArgumentException("Command line arguments could not be parsed ...");
+            }
+            
+            Console.WriteLine(options.Test);
+
+            Console.WriteLine("Press ENTER to continue ...");
+            Console.ReadLine();
+        }
+    }
+}
