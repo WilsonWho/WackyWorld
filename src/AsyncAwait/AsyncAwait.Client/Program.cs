92699f3c4b6cbda36fbb19e1b9944cae3afb36d7
wilsohn <Wilson.Tzou@iqmetrix.com>
2014-09-04 17:21:13 -0700

    Adding a very basic async/await example

---
+++ src/AsyncAwait/AsyncAwait.Client/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using AsyncAwait.Server;
+
+namespace AsyncAwait.Client
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            const int value = 5;
+            var blackbox = new BlackBox();
+
+            var result = blackbox.GetValue(value);
+            Console.WriteLine(result);
+
+            var resultAsync = blackbox.GetValueAsync(value).Result;
+            Task.WaitAll();
+            Console.WriteLine(resultAsync);
+
+            Console.ReadKey();
+        }
+    }
+}
